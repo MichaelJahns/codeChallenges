2981e0042dbfa2a4a17d283c9e1cc30589b4e6f7
MichaelJahns <jahnsmic001@gmail.com>
2019-04-17 17:00:20 -0700

    Solution and test cases

--- java/binarySearch/src/main/java/binarySearch/App.java
+++ java/binarySearch/src/main/java/binarySearch/App.java
@@ -3,12 +3,20 @@
  */
 package binarySearch;
 
-public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public class App {
+        public static void main(String[] args) {
+            binarySearch(4, new int[] {1,2,3,4});
+        }
+
+        public static int binarySearch(int target, int[] input) {
+            for (int i = 0; i < input.length; i++) {
+                if (input[i] == target) {
+                    System.out.println(i);
+                    return i;
+                }
+
+            }
+            return -1;
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
-}
