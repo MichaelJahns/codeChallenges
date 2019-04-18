1be029433f3247088b2f402b9a99786cb6a015c0
MichaelJahns <jahnsmic001@gmail.com>
2019-04-17 17:35:50 -0700

    Adds benchmark stretch goals

--- java/binarySearch/src/test/java/binarySearch/AppTest.java
+++ java/binarySearch/src/test/java/binarySearch/AppTest.java
@@ -6,10 +6,11 @@ package binarySearch;
 import org.junit.Test;
 
 import static binarySearch.App.binarySearch;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class AppTest {
-    @Test public void testBinarySearch(){
+    @Test
+    public void testBinarySearch() {
         int[] testArray1 = {1, 2, 3, 4, 5};
         int[] testArray2 = {10, -30, 44, 0};
         //expected
@@ -21,5 +22,26 @@ public class AppTest {
         //edge
         int[] edgeArray = {0, 0, 0, 0, 14};
         assertEquals(0, binarySearch(0, edgeArray));
+    }
+
+    @Test
+    public void benchMarkBinarySearch() {
+        int[] benchMark100 = new int[100];
+        int[] benchMark10000 = new int[10_000];
+        int[] benchMark100000 = new int[100_000];
+
+        for (int i = 0; i < 100; i++) {
+            benchMark100[i] = i;
+        }
+        for (int i = 0; i < 10_000; i++) {
+            benchMark10000[i] = i + 1;
+        }
+        for (int i = 0; i < 100_000; i++) {
+            benchMark100000[i] = i + 1;
+        }
+        assertEquals(99, binarySearch(99, benchMark100));
+        assertEquals(9999, binarySearch(10000, benchMark10000));
+        assertEquals(99999, binarySearch(100000, benchMark100000));
+
     }
 }
